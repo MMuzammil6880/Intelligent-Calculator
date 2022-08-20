127662fe7437f20220ceaf76cdd50d8ad86fbbdd
Muhammad Muzammil <muhammadmuzammil6880@gmail.com>
2022-08-19 17:13:26 -1000

    intial commit

--- TipCalculator.js
+++ TipCalculator.js
@@ -12,9 +12,10 @@ let numberOfPeoples = Number(numOfPerson.innerText)
 const calculateBill = () => {
     // get bill from user input & convert it into a number
     const billAmountTotal = Number(billAmount.value)
+  
     // get the tip from user & convert it into a percentage 
     const tipPercentage = Number(tipAmount.value) / 100
-  
+    
     const tipTotal = tipPercentage * billAmountTotal
     const Total = tipTotal + billAmountTotal
     let perPersonAmount = Total / numberOfPeoples
@@ -22,7 +23,7 @@ const calculateBill = () => {
     // update the perPersonTotal on DOM & show it to user
     totalBillAmount.innerText = `Rs ${Total}`
     perPersonTotal.innerText = `Rs ${perPersonAmount.toFixed(2)}`
-  
+
   }
   
   
@@ -52,4 +53,23 @@ const calculateBill = () => {
   
     calculateBill()
   
+  }
+
+  const finishCalculation = () => {
+
+  //  let finishButton = document.getElementById('finish')
+      let billTotal = document.getElementById('totalBillAmount')
+      let billAmount = document.getElementById('billTotalInput')
+      let tipAmount = document.getElementById('tipInput')
+      let numOfPerson = document.getElementById('numberOfPeople')
+      let perPersonTotal = document.getElementById('perPersonTotal')
+     
+      
+      billTotal.innerText =" Rs 0.00"
+      billAmount.innerText = 0
+      tipAmount.innerText  =  0
+      numOfPerson.innerText = 1
+      perPersonTotal.innerText = "Rs 0.00"
+
+    
   }
